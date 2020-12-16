0305a62757ba9db2f0b97467d27541429647da2b
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 13:15:17 -0500

    What BubbleSort looks like near the end of class-to be tested to see if it works as it should

--- Sorts.java
+++ Sorts.java
@@ -2,16 +2,17 @@ public class Sorts{
 
   public static void bubbleSort(int[] data){
     int lastcheck = 0;
-    for(int i = 0; i < data.length - 1; i++){
+    for(int i = 0; i < data.length - lastcheck; i++){
       if(data[i] > data[i + 1]){
-        for(int j = i; j < data.length - 1; j++){
+        for(int j = i; j < data.length - lastcheck; j++){
           if(data[j] > data[j + 1]){
             int swapper = data[j + 1];
             data[j + 1] = data[j];
             data[j] + swapper;
                          }
                        }
-                     }
+            lastcheck = lastcheck + 1;
+                    }
               }
     }
 
