a8d407f03eceb738c0304cdb06d1b1258234466f
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 21:09:48 -0500

    made debugging it's own separate class

---
+++ Debug.java
@@ -0,0 +1,10 @@
+public class Debug{
+  public static void dprint(int[] data){
+    public static boolean DEBUG = true;
+    if(DEBUG){
+      System.out.println(data.toString());
+    }
+  }
+
+
+}
