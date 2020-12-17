e2ee2c9bc003df3f212e2690eae2a4658ab3188d
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 21:02:56 -0500

    Made a first try of bubblesorts with debug now, so the steps can be followed as it compiles and runs

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
 public class Sorts{
 
+  public static void dprint(int[] data){
+    public static boolean DEBUG = true;
+    if(DEBUG){
+      System.out.println(data.toString());
+    }
+  }
+
   public static void bubbleSort(int[] data){
+
     int lastcheck = 0;
     for(int i = 0; i < data.length - lastcheck; i++){
       if(data[i] > data[i + 1]){
@@ -10,6 +19,7 @@ public class Sorts{
             data[j + 1] = data[j];
             data[j] + swapper;
                          }
+            dprint(data);
                        }
             lastcheck = lastcheck + 1;
                     }
