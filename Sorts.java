736356df5113ce23bc00678b25889d047df0def8
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 21:27:44 -0500

    Modified the bubbleSorts so it would compile, and return to a previous index if a number gets swapped over to the end

--- Sorts.java
+++ Sorts.java
@@ -1,28 +1,22 @@
-import java.util.Arrays;
 public class Sorts{
-
-  public static void dprint(int[] data){
-    public static boolean DEBUG = true;
-    if(DEBUG){
-      System.out.println(data.toString());
-    }
-  }
 
   public static void bubbleSort(int[] data){
 
     int lastcheck = 0;
     for(int i = 0; i < data.length - lastcheck; i++){
       if(data[i] > data[i + 1]){
-        for(int j = i; j < data.length - lastcheck; j++){
+        for(int j = i; j < data.length - lastcheck - 1; j++){
           if(data[j] > data[j + 1]){
             int swapper = data[j + 1];
             data[j + 1] = data[j];
-            data[j] + swapper;
+            data[j] = swapper;
                          }
-            dprint(data);
+            Debug.dprint(data);
                        }
             lastcheck = lastcheck + 1;
+            Debug.dreturn(lastcheck);
                     }
+            i = i - 1;
               }
     }
 
