9651814012cb86993d87a0890fe2abcbf4a1250f
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 22:48:33 -0500

    I don't remember If there were any changes, but just in case there was, and bubbleSort seems to be done

--- Sorts.java
+++ Sorts.java
@@ -3,15 +3,15 @@ public class Sorts{
   public static void bubbleSort(int[] data){
 
     int lastcheck = 0;
-    for(int i = 0; i < data.length - lastcheck; ){
+    for(int i = 0; i < data.length - lastcheck - 1; ){
       if(data[i] > data[i + 1]){
         for(int j = i; j < data.length - lastcheck - 1; j++){
           if(data[j] > data[j + 1]){
             int swapper = data[j + 1];
             data[j + 1] = data[j];
             data[j] = swapper;
-                         }
             Debug.dprint(data);
+                         }
                        }
             i = 0;
             lastcheck = lastcheck + 1;
@@ -21,3 +21,4 @@ public class Sorts{
        }
     }
   }
+//:]
