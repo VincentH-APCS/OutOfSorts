f0b3ece70c7a5ed440c5df046a8a0350d00894d3
VincentH-APCS <vhuang10@stuy.edu>
2020-12-16 21:54:38 -0500

    Added a more complex array to sort

--- Tester.java
+++ Tester.java
@@ -1,9 +1,10 @@
 public class Tester{
   public static void main(String[] args){
     int[] stats = {6,3,2,1,4,5};
+    int[] numbas ={98,7,62,0,3,2,1056, -2, 20, -1,43,52,11,43,9};
 
-   Sorts.bubbleSort(stats);
-   //System.out.println(stats.length);
+   //Sorts.bubbleSort(stats);
+   Sorts.bubbleSort(numbas);
   }
 
 }
