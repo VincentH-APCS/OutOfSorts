f81216ba5f8e34e3a4856bb3df19c33871104c83
VincentH-APCS <vhuang10@stuy.edu>
2020-12-18 13:19:15 -0500

    Time to test selectionSort using basic, yet varied test cases to make sure the basics of it work

--- Tester.java
+++ Tester.java
@@ -4,7 +4,7 @@ public class Tester{
 
   public static void RAmaker(int[] arry){
     Random rng = new Random();
-    rng = new Random(55);
+    rng = new Random(43);
     for(int i = 0; i < arry.length; i++){
       arry[i] = rng.nextInt() % 1000;
     }
@@ -12,12 +12,17 @@ public class Tester{
 
   public static void main(String[] args){
     int[] stats = {6,3,2,1,4,5};
+    int[] nothing = {};
+    int[] zeroes = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int[] reversed = {7,6,5,4,3,2,1};
     int[] numbas ={98,7,62,0,3,2,1056, -2, 20, -1,43,52,11,43,9};
     int[] randoms = new int[20];
-   //Sorts.bubbleSort(stats);
-  // Sorts.bubbleSort(numbas);
-  RAmaker(randoms);
-  System.out.println(randoms.length);
-  Sorts.bubbleSort(randoms);
+
+  //RAmaker(randoms);
+  Sorts.selectionSort(nothing);
+  Sorts.selectionSort(zeroes);
+  Sorts.selectionSort(reversed);
+  Sorts.selectionSort(stats);
+
 }
 }
